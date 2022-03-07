715f9254d7a39d23fc14f16f20a4f76c96ded5ea
hammy3502 <hammy275@gmail.com>
2022-03-06 23:34:56 -0500

    Immersive Furnace (closes #4)

---
+++ src/main/java/net/blf02/vrplayground/common/util/PlayerTracker.java
@@ -0,0 +1,13 @@
+package net.blf02.vrplayground.common.util;
+
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PlayerTracker {
+
+    public static final Set<String> handsInFurnace = new HashSet<>();
+}
