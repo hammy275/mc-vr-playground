06e5aee0df89b885edd4e321563112719be9ec6d
hammy3502 <hammy275@gmail.com>
2022-03-07 23:56:53 -0500

    Rocket Hands Particles (closes #3)

--- src/main/java/net/blf02/vrplayground/common/item/RocketHands.java
+++ src/main/java/net/blf02/vrplayground/common/item/RocketHands.java
@@ -1,14 +1,17 @@
 package net.blf02.vrplayground.common.item;
 
+import net.blf02.vrapi.api.data.IVRData;
 import net.blf02.vrapi.api.data.IVRPlayer;
 import net.blf02.vrplayground.common.vr.VRPlugin;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 public class RocketHands extends Item {
 
@@ -35,6 +38,27 @@ public class RocketHands extends Item {
         // If we're currently moving up, cancel our fall damage
         if (player.getDeltaMovement().y >= 0) {
             player.fallDistance = 0; // Reset fall distance if we're going up
+        }
+
+        if (!level.isClientSide) {
+            ServerWorld serverLevel = (ServerWorld) level;
+            for (int i = 0; i <= 1; i++) {
+                IVRData controller = vrPlayer.getController(i); // Get controller number i (0 and 1)
+                Vector3d pos = controller.position(); // Get the position of the controller
+                Vector3d look = controller.getLookAngle(); // Get the angle the controller is pointing
+                serverLevel.sendParticles(ParticleTypes.SMOKE, // Add smoke particles
+                        pos.x, pos.y, pos.z, // At the position of our controllers
+                        4, // Adding 4 particles
+                        look.x, look.y, look.z, // Spreading out in the direction of our controller
+                        0.05 // Moving at the speed 0.05
+                        );
+                serverLevel.sendParticles(ParticleTypes.FLAME, // Add a flame particle
+                        pos.x, pos.y, pos.z, // At the position of the controller
+                        1, // Only one particle
+                        look.x, look.y, look.z, // Spreading out in the direction of our controller
+                        0); // Make the particle not move
+            }
+
         }
         return ActionResult.success(player.getItemInHand(hand));
     }
