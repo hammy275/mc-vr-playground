7e2151c981b6d7fe3fb56abb413723e5e0bb4c9d
hammy275 <hammy275@gmail.com>
2025-06-22 21:34:37 -0400

    Update for Registration Event

--- common/src/main/java/com/hammy275/mcvrplayground/MCVRPlaygroundClient.java
+++ common/src/main/java/com/hammy275/mcvrplayground/MCVRPlaygroundClient.java
@@ -11,7 +11,7 @@ public class MCVRPlaygroundClient {
     public static final ShieldLookTracker shieldLookTracker = new ShieldLookTracker();
 
     public static void init() {
-        VRClientAPI.instance().registerTracker(shieldLookTracker);
+        VRClientAPI.instance().addRegistrationHandler(event -> event.registerTracker(shieldLookTracker));
 
         EntityRendererRegistry.register(ModEntities.magicMissile, ScaledItemRenderer::new);
         EntityRendererRegistry.register(ModEntities.energyBall, ScaledItemRenderer::new);
